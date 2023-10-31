16d0192c647d8a26b801862653d1bedc840e4f4b
Nenza Nurfirmansyah <nenza.dev@gmail.com>
2023-10-31 13:16:49 +0700

    add widget title

---
+++ lib/widgets/text_title_block.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TextTitleBlock extends StatelessWidget {
+  final String title;
+  const TextTitleBlock({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(15, 11, 0, 11),
+      margin: EdgeInsets.symmetric(vertical: 16),
+      width: double.maxFinite,
+      color: Colors.blueAccent,
+      child: Text(
+        title,
+        style: const TextStyle(
+            fontSize: 12, fontWeight: FontWeight.w700, color: Colors.white),
+      ),
+    );
+  }
+}
